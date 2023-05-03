dfc98940fb20cc04183d49d335af97ac0981a71f
MathiasHjorth <mathiasbirgerhjorth95@gmail.com>
2023-05-03 14:49:00 +0200

    moved controller tests to api/v1

--- test/controllers/api/v1/sessions_controller_test.rb
+++ test/controllers/api/v1/sessions_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class Api::V1::SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # Setting up a user
+    post '/signup', params:{user:{name: 'John', email: 'user10@user.dk', password: '1234567'}}
+  end
+
+  test "should return an authorization header on successful logins" do
+    post '/login', params:{
+      user:{
+        email: 'user10@user.dk',
+        password: '1234567'
+      }
+    }
+    assert_response 200, 'should get a 200 response on successful logins'
+    assert_equal false, @response['Authorization'].nil?
+  end
+
+  test "request params not formed according to sign_in_params should fail" do
+    assert_raises ActionController::ParameterMissing do
+      post '/login', params:{
+        usera:
+          {
+            email: 'user10@user.dk'
+          }
+      }
+    end
+  end
 end
